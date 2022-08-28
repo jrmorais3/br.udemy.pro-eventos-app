520059e6f07560f54c4ccb53255b99819b32028b
Junior Morais <jrmorais3@gmail.com>
2022-08-27 22:01:56 -0300

    first commit

---
+++ back/src/proeventos.api/Controllers/EventoController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace proeventos.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }   
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+
+    }
+}
